d78ac2ec1ce4be62b24fdd01a78a84058538da67
manysin <s.many@dgc.gov.kh>
2024-06-20 13:34:01 +0700

    card UI

---
+++ app/src/main/java/com/sinmany/e_commerce/Activity/CartActivity.java
@@ -0,0 +1,15 @@
+package com.sinmany.e_commerce.Activity;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.sinmany.e_commerce.R;
+
+public class CartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cart);
+    }
+}
