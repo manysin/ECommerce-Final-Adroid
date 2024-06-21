b6d25c1389b94cb4ebe133ef4a2bab055ced673d
manysin <s.many@dgc.gov.kh>
2024-06-21 15:15:21 +0700

    added order thank you page

--- app/src/main/java/com/sinmany/e_commerce/Activity/EndOrderActivity.java
+++ app/src/main/java/com/sinmany/e_commerce/Activity/EndOrderActivity.java
@@ -1,5 +1,6 @@
 package com.sinmany.e_commerce.Activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import com.sinmany.e_commerce.databinding.ActivityEndOrderBinding;
@@ -12,10 +13,15 @@ public class EndOrderActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityEndOrderBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        buttonNameNavigation();
         setVariable();
     }
 
     private void setVariable() {
         binding.backBtn.setOnClickListener(v -> finish());
+    }
+
+    private void buttonNameNavigation() {
+        binding.btnBackHome.setOnClickListener(v -> startActivity(new Intent(EndOrderActivity.this, MainActivity.class)));
     }
 }
